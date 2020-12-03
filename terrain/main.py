31807e6e0e28fadeff208de10fe1a49b4b7006d6
Andy Bryant <a.mcdermid.bryant@gmail.com>
2020-12-03 16:36:51 -0500

    finished serializing

--- terrain/main.py
+++ terrain/main.py
@@ -7,19 +7,6 @@ from actor import Actor
 from action import Action
 from event import Event
 
-# t = Terrain()
-
-# data = t.serialize()
-# with open('data.json', 'w') as f:
-    # json.dump(data, f)
-
-# Create one terrain
-# Create 4 random domains
-# Create 200 random venues
-# Create 2000 random actions
-# Create 100 random people
-# Create events that link action with venue
-
 terrain = Terrain()
 domains = [Domain() for _ in range(4)]
 
@@ -40,11 +27,16 @@ for _ in range(2000):
     events.append(new_event)
 
 data = {
-    "terrain": terrain.serialize(),
-    "domains": [d.serialize() for d in domains],
-    "venues": [v.serialize() for v in venues],
-    "actors": [a.serialize() for a in actors],
-    "events": [e.serialize() for e in events]
+    "scenario": {
+        "name": "cybersocial1",
+    },
+    "data": {
+        "terrain": terrain.serialize(),
+        "domains": [d.serialize() for d in domains],
+        "venues": [v.serialize() for v in venues],
+        "actors": [a.serialize() for a in actors],
+        "events": [e.serialize() for e in events]
+    }
 }
 
 with open('data.json', 'w') as f:
